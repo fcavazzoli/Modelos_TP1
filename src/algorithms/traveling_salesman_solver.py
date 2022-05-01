55dedac63c81a721f453e51c1927922d249874d2
Federico <macbookpro@MacBooks-MacBook-Pro.local>
2022-05-01 18:06:41 -0300

    total: 9575.350015520653

--- src/algorithms/traveling_salesman_solver.py
+++ src/algorithms/traveling_salesman_solver.py
@@ -1,2 +1,27 @@
-def solve(matrix):
-    pass
+from locale import currency
+
+
+def can_go_to_city(city_weights, city, amount):
+    return amount + city_weights[city] > 0
+
+
+def solve(matrix, city_weights):
+    solution = [0]
+    city = 0
+    min_city = 0
+    current_amount = city_weights[0]
+    total_distance = 0
+    while len(solution) < len(matrix):
+        min_distance = float("inf")
+        for i in range(len(matrix)):
+            if (i not in solution) and can_go_to_city(city_weights, i, current_amount):
+                if matrix[city][i] < min_distance:
+                    min_distance = matrix[city][i]
+                    min_city = i
+        solution.append(min_city)
+        city = min_city
+        current_amount += city_weights[min_city]
+        total_distance += min_distance
+    # Primer item es 0 y deberia ser 1, ultimo item es 149 y deberia ser 150.
+    print("total_distance:", total_distance)
+    return [x + 1 for x in solution]
--- src/main.py
+++ src/main.py
@@ -7,8 +7,7 @@ data = 'datasets/data.txt'
 model = parse_file(data)
 adjacency_matrix = model.adjacency_matrix()
 
-solution = solver.solve(adjacency_matrix)
+solution = solver.solve(adjacency_matrix, model.requests)
 
 with open('soluciones/solucion_1.txt', 'w') as f:
-    for row in adjacency_matrix:
-        f.write(' '.join(map(str, row)) + '\n')
+    f.write(str(solution))
