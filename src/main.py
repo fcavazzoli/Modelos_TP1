39be01f6b156f46cbe59bde989505a787e19fed3
Federico <macbookpro@MacBooks-MacBook-Pro.local>
2022-05-02 22:50:48 -0300

    recupero el primer archivo de solucion

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from helpers.parser import parse_file
 import algorithms.brut_force as bf
+import algorithms.traveling_salesman_solver as tsm
 
 data = 'datasets/data.txt'
 
 model = parse_file(data)
 adjacency_matrix = model.adjacency_matrix()
 
-solution = bf.solve(adjacency_matrix, model.requests)
+solution = tsm.solve(adjacency_matrix, model.requests, 0)
 
-with open('soluciones/solucion_1.txt', 'w') as f:
-    f.write(' '.join(solution))
+with open('soluciones/solucion_2.txt', 'w') as f:
+    f.write(' '.join(solution[1]))
